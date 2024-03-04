ac9fd506fbc02a2e6ff53370e6219fb03f2bafe7
esurajpandey <esurajpandey@gmail.com>
2024-03-04 22:31:14 +0530

    Method headers updated

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
-app.use(cors({
-    origin: '*',
-}));
+app.use(
+    cors({ 
+        origin: [
+            'http://localhost:3000',
+             /\.regenci\.online$/],
+             credentials: true 
+    })
+)
 
 const PORT = process.env.PORT || 9000;
 
