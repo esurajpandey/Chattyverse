64e9357a4a71195c5ca40a699fa9a8ba9dd50778
esurajpandey <esurajpandey@gmail.com>
2024-03-04 21:45:48 +0530

    Server configuration done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+app.use(cors({
+    origin: '*',
+}));
+
+const PORT = process.env.PORT || 9000;
+
+app.get('/api/health', async(req, res) => {
+    res.status(200).send({
+        data : null,
+        message : "Backend server is running fine",
+        success : true,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
